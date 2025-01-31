83ddea9439edc301641dbb628c519b03182b70d9
wan1yan <149595034+wan1yan@users.noreply.github.com>
2025-01-31 12:38:14 +0800

    Create modifyUserName.js

---
+++ modifyUserName.js
@@ -0,0 +1,18 @@
+const CUSTOM_USERNAME = "MyCustomName"; // 想改成什么就自己填
+
+(() => {
+  let body = $response.body;
+
+  try {
+    let obj = JSON.parse(body);
+    // 2. 修改字段
+    if (obj.ServerInfo && obj.ServerInfo.UserName) {
+      obj.ServerInfo.UserName = CUSTOM_USERNAME;
+    }
+    body = JSON.stringify(obj);
+  } catch (e) {
+    console.log("脚本解析出错: " + e);
+  }
+
+  $done({ body });
+})();
