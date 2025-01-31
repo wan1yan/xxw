244686589eaf62d368ab2d382fc638459ee536d9
wan1yan <149595034+wan1yan@users.noreply.github.com>
2025-01-31 13:13:36 +0800

    更新 modifyUserName.js

--- modifyUserName.js
+++ modifyUserName.js
@@ -1,17 +1,21 @@
-
 (() => {
-  let body = $response.body;
+    let res = $response;  // 直接获取整个响应对象
+    let body = res.body;
 
-  try {
-    let obj = JSON.parse(body);
-    // 2. 修改字段
-    if (obj.ServerInfo && obj.ServerInfo.UserName) {
-      obj.ServerInfo.UserName = "sid77844000";
+    try {
+        let obj = JSON.parse(body);  // 解析 JSON 数据
+
+        // 确保 ServerInfo 存在，并修改 UserName
+        if (obj && obj.ServerInfo && typeof obj.ServerInfo === "object") {
+            obj.ServerInfo.UserName = "sid77844000";
+        } else {
+            console.log("ServerInfo 结构不匹配，无法修改 UserName");
+        }
+
+        res.body = JSON.stringify(obj);  // 重新序列化 JSON 并赋值给 res.body
+    } catch (e) {
+        console.log("JSON 解析出错: " + e);
     }
-    body = JSON.stringify(obj);
-  } catch (e) {
-    console.log("脚本解析出错: " + e);
-  }
 
-  $done({ body });
+    $done(res);  // 直接返回修改后的整个响应对象
 })();
